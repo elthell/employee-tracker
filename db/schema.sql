2f51c1f172b48d0d9315ae2e372543adbf971953
elthell <eleanor.thell@gmail.com>
2023-09-04 18:44:40 -0500

    add starter code for server + schema

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+-- departments table --
+
+-- roles table --
+
+-- employees table --
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: 'example',
+      database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+  );
+
+  // questions
+
+  // async function to start questions
+
+  // Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
